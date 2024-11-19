64ad42a196c6349a14f856286e971e6f8088f96a
Tung Le <lenhattung@gmail.com>
2024-11-19 15:56:20 +0700

    Create ex06.dart

---
+++ src/nhom_31/p1/ex06.dart
@@ -0,0 +1,10 @@
+/**
+Bài tập:
+1. Viết chương trình chuyển đổi 1 số 
+nguyên (b>=1) được nhập từ bàn phím 
+thành số nhị phân.
+
+2. Viết chương trình tính tổng
+ các ước số của một số nguyên được 
+ nhập từ bàn phím (n>=1). 
+ */
