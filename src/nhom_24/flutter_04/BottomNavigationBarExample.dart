2e0560147b9859c45be08b0253d07d84c82be86c
Tung Le <lenhattung@gmail.com>
2024-12-02 14:12:00 +0700

    Update BottomNavigationBarExample.dart

--- src/nhom_24/flutter_04/BottomNavigationBarExample.dart
+++ src/nhom_24/flutter_04/BottomNavigationBarExample.dart
@@ -1,3 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_01/baitap1.dart';
+
+class CardExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Card Example'),
+        ),
+        body: Center(
+          child: Card(
+            elevation: 4.0,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text('This is a card'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+//
+class CircleAvatarExample extends StatelessWidget {
+  const CircleAvatarExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Circle Avatar Example'),
+        ),
+        body: Center(
+          child: CircleAvatar(
+            radius: 50,
+            backgroundImage: NetworkImage(
+                'https://picsum.photos/200/200'
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//
+class BorderExample extends StatelessWidget {
+  const BorderExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('BorderExample'),
+        ),
+        body: Center(
+          child: Container(
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.blue,
+                  width: 3,
+                )
+            ),
+            child: const Center(
+              child: Text('Border Example'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+///
 
 class BottomNavigationBarExample extends StatefulWidget {
   const BottomNavigationBarExample({super.key});
@@ -9,6 +91,12 @@ class BottomNavigationBarExample extends StatefulWidget {
 //
 class _BottomNavigationBarExampleState extends State<BottomNavigationBarExample> {
   int _selectedIndex = 0;
+  // Sử dụng các class có sẵn
+  static final List<Widget> _widgetOptions = <Widget>[
+    const BorderExample(),
+    const CircleAvatarExample(),
+    const ProfilePage(),
+  ];
   // Hàm xử lý khi tap vào item
   void _onItemTapped(int index) {
     setState(() {
@@ -23,9 +111,7 @@ class _BottomNavigationBarExampleState extends State<BottomNavigationBarExample>
         appBar: AppBar(
           title: Text('BottomNavigationBar'),
         ),
-        body: Center(
-
-        ),
+        body: _widgetOptions.elementAt(_selectedIndex),
         bottomNavigationBar: BottomNavigationBar(
           items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(
