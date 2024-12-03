358be8c91c2241f0898f7ff4b3e7cc85e68ca5cf
Tung Le <lenhattung@gmail.com>
2024-12-03 14:33:41 +0700

    Update flutter_01.dart

--- src/nhom_31/flutter_01/flutter_01.dart
+++ src/nhom_31/flutter_01/flutter_01.dart
@@ -54,3 +54,85 @@ class RowExample extends StatelessWidget{
     );
   }
 }
+
+class ColumnExample extends StatelessWidget{
+  const ColumnExample({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Column Example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(Icons.star),
+            Icon(Icons.add),
+            Icon(Icons.access_alarm),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class StackExample extends StatelessWidget{
+  const StackExample({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stack Example'),
+      ),
+      body: Center(
+        child: Stack(
+          children: <Widget>[
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+            Container(
+              width: 50,
+              height: 50,
+              color: Colors.blue,
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class ContainerExample extends StatelessWidget {
+  const ContainerExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stack Example'),
+      ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          color: Colors.blue,
+          child: Text('Hello Container!'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
